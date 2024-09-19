7d6d3a7c2f5ebb92f05cfa47350e7f6b63883a79
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-09-19 16:52:27 +0700

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "airbnb_like_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'heavennest-7miq.onrender.com' }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
