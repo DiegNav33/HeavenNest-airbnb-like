f7755c9b48cc133773f6553de113244735d974ac
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-09-19 17:27:27 +0700

    Confirmable removed for test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "airbnb_like_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'heavennest-7miq.onrender.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.sendgrid.net',
+  #   port:                 587,
+  #   domain:               'yourdomain.com',
+  #   user_name:            'apikey', # pour SendGrid
+  #   password:             'your_sendgrid_api_key',
+  #   authentication:       :plain,
+  #   enable_starttls_auto: true
+  # }
+
+  config.action_mailer.default_url_options = { host: 'your-production-domain.com' }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
